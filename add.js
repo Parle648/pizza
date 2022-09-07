9d280385c887a77dbe30b7f0a96bf7d65a27a6b9
Parle648 <sashahoumi@gmail.com>
2022-09-07 11:01:39 +0300

    init: add index, css, js + create Carousel

---
+++ add.js
@@ -0,0 +1,54 @@
+const BtnPlas = document.querySelector('.plas');
+const sliders = document.querySelector('.carousel');
+
+const SliderNext = function(){
+
+    const arrBlocks = document.querySelectorAll('.sliders');
+
+    let newObj = document.createElement('div');  newObj.classList.add('sliders')
+
+    let img = document.createElement('img');
+    img.classList.add('sliderImg'); img.src = arrBlocks[0].firstElementChild.getAttribute('src')
+
+    sliders.appendChild(newObj); newObj.appendChild(img)
+
+    sliders.classList.add('animationNext'); 
+    
+    sliders.addEventListener('animationend', function(){
+
+    sliders.classList.remove('animationNext')
+
+    arrBlocks[0].remove()
+})
+}
+
+    const btnPrevious = document.querySelector('.minus')
+
+    const SluderPrevious = function(){
+        const arrBlocks = document.querySelectorAll('.sliders');
+
+        let FirstBlock = document.createElement('div');FirstBlock.classList.add('sliders');FirstBlock.classList.add('absolute')
+
+        let img = document.createElement('img'); img.classList.add('sliderImg'); img.src = sliders.lastElementChild.firstElementChild.getAttribute('src')
+
+        FirstBlock.appendChild(img); sliders.prepend(FirstBlock);
+
+        sliders.classList.add('animPrevious')
+
+        sliders.addEventListener('animationend', function(){
+
+            sliders.classList.remove('animPrevious');FirstBlock.classList.remove('absolute')
+        
+            arrBlocks[3].remove()
+        })
+    }
+
+BtnPlas.addEventListener('click', SliderNext);
+btnPrevious.addEventListener('click', SluderPrevious)
+
+const arr1 = [1, 2, 3]
+const arr2 = [2, 6, 7]
+
+const arr = arr1.concat(arr2)
+
+console.log(arr)
